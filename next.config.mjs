b4b53d8381bf9b0ff44dbb82d6fd5de6e1701689
mohammadJahid8 <mohammadjahid0007@gmail.com>
2025-05-12 06:11:35 +0600

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "b.urinvited.io",
+          },
+        ],
+        destination: "https://urinvited.io/:path*",
+        permanent: true,
+      },
+    ];
+  },
   images: {
-    domains: ['wp1.themevibrant.com', 'res.cloudinary.com'],
+    domains: ["wp1.themevibrant.com", "res.cloudinary.com"],
   },
 };
 
